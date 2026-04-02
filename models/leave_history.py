3a764780788e58273bdd10996b44c8ae1179ab8c
Tushar <tusharshejwal1803@gmail.com>
2026-04-02 12:14:34 +0530

    Riya user service and schema

---
+++ models/leave_history.py
@@ -0,0 +1,5 @@
+class LeaveHistory:
+    def __init__(self, request_id, action_taken, action_taken_by):
+        self.request_id = request_id
+        self.action_taken = action_taken
+        self.action_taken_by = action_taken_by
